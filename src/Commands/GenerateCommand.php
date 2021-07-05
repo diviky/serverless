ee0ed911014f7a1ad6b76b536a5fad5b863c9a10
Sankar Suda <sankar.suda@gmail.com>
2021-07-05 13:30:43 +0530

    s3 url fixed

---
+++ src/Commands/GenerateCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Diviky\Serverless\Commands;
+
+use Diviky\Serverless\Concerns\ExecuteTrait;
+use Diviky\Serverless\Serverless;
+use Laravel\VaporCli\Commands\BuildCommand as VaporBuildCommand;
+use Laravel\VaporCli\Helpers;
+use Symfony\Component\Console\Input\InputArgument;
+
+class GenerateCommand extends VaporBuildCommand
+{
+    use ExecuteTrait;
+
+    /**
+     * Execute the command.
+     */
+    public function handle()
+    {
+        Serverless::generate();
+
+        Helpers::line();
+        Helpers::line('<info>Serverless file created successfully.</info>');
+    }
+
+    /**
+     * Configure the command options.
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('generate')
+            ->addArgument('environment', InputArgument::OPTIONAL, 'The environment name', 'staging')
+            ->setDescription('Create Serverless file');
+    }
+}
